b988c9ff1164da50b6d83532ab4ecbffb6787a2a
Keaton Conrad <keaton.conrad@gmail.com>
2022-08-28 12:58:48 -0500

    Chapter 14

---
+++ src/chunk.h
@@ -0,0 +1,25 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count; // How many allocated entries are actually in use
+    int capacity;  // The number of elements in the array we've allocated
+    uint8_t* code;
+    int* lines; // Line numbers corresponding to the bytecode instructions
+    ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+
+#endif
